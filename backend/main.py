c5e940bb4b8096a5f49e382f7f90c73d7dbb6264
kathyjydong <kathyjydong@gmail.com>
2026-01-10 16:12:53 -0500

    gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,43 @@
-print("hello world")
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=2,
+    min_detection_confidence=0.6,
+    min_tracking_confidence=0.6
+)
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    result = hands.process(rgb)
+
+    if result.multi_hand_landmarks:
+        for hand_landmarks in result.multi_hand_landmarks:
+            for i, lm in enumerate(hand_landmarks.landmark):
+                print(i, lm.x, lm.y, lm.z)
+
+            mp_draw.draw_landmarks(
+                frame,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS
+            )
+
+    cv2.imshow("Hand Backend", frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
